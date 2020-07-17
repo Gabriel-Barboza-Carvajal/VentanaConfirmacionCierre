ef6b27bea4aab8f2f75c6e37a8d9eb873de915b0
gabriel-barboza <gabriel@DESKTOP-R52JBAU>
2020-07-16 20:08:04 -0600

    Ejemplo de como confirmar el cierre de una ventana

---
+++ src/ventanaconfirmacioncierre/VentanaConfirmacionCierre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanaconfirmacioncierre;
+
+/**
+ *
+ * @author gabri
+ */
+public class VentanaConfirmacionCierre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        VentanaInformacion ven=new VentanaInformacion();
+        
+        
+    }
+    
+}
